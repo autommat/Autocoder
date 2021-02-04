af978f84c65f75af2fd6c3eebbbf72d963344346
PiotrekWardecki <piotrekwardecki2@gmail.com>
2021-02-04 14:57:27 +0100

    better plots

--- plots.py
+++ plots.py
@@ -4,5 +4,8 @@ import matplotlib.pyplot as plt
 if __name__ == '__main__':
     for i in range(1,9):
         df = pd.read_csv(f"out/img{i}.csv", index_col=0)
-        plt.plot(df)
+        plt.plot(df, label='image '+str(i))
+        plt.ylabel('PSNR')
+        plt.xlabel('compression degree')
+        plt.legend()
     plt.savefig(f"out/plot.jpg")
