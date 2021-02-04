be7f6f996afec77ad5db3415d056690bf7a7709e
PiotrekWardecki <piotrekwardecki2@gmail.com>
2021-02-04 15:04:58 +0100

    added marker to plot

--- plots.py
+++ plots.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 if __name__ == '__main__':
     for i in range(1,9):
         df = pd.read_csv(f"out/img{i}.csv", index_col=0)
-        plt.plot(df, label='image '+str(i))
+        plt.plot(df,marker='o', label='image '+str(i))
         plt.ylabel('PSNR')
         plt.xlabel('compression degree')
         plt.legend()
